c924ae37131a561a87f5ad15b273f92d602392c3
romalytv <youlikeyoupass@gmail.com>
2025-01-26 14:58:11 +0200

    fix: create clean project

---
+++ src/main/java/proj/hammr/hammr/HammrApplication.java
@@ -0,0 +1,13 @@
+package proj.hammr.hammr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HammrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proj/hammr/hammr/HammrApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.hammr.hammr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HammrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
